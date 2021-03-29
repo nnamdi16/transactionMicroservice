82ecaa1e6028b9d19252590763efdd334d6a765b
Nnamdi <nnwabuokei@paga.com>
2021-03-29 15:10:09 +0100

    Implemented the TransactionRequest payload that acts as a blue print for call the transaction endpoint.

--- src/main/java/com/nnamdi/transaction/payload/request/TransactionRequest.java
+++ src/main/java/com/nnamdi/transaction/payload/request/TransactionRequest.java
@@ -1,16 +1,20 @@
 package com.nnamdi.transaction.payload.request;
 
+import java.util.Date;
+
 public class TransactionRequest {
     private String transactionType;
     private Double amount;
     private String transactionDescription;
     private Long accountId;
+    private Date date;
 
-    public TransactionRequest(String transactionType, Double amount, String transactionDescription, Long accountId) {
+    public TransactionRequest(String transactionType, Double amount, String transactionDescription, Long accountId, Date date) {
         this.transactionType = transactionType;
         this.amount = amount;
         this.transactionDescription = transactionDescription;
         this.accountId = accountId;
+
     }
 
     public String getTransactionType() {
@@ -43,5 +47,13 @@ public class TransactionRequest {
 
     public void setAccountId(Long accountId) {
         this.accountId = accountId;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = new Date();
     }
 }
