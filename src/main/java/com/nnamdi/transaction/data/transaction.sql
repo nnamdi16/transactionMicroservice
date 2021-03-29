54502b17dfc90ecda4fb42f8fea9752671da0a21
Nnamdi <nnwabuokei@paga.com>
2021-03-29 15:03:48 +0100

    Sql script for creating the database database

--- src/main/java/com/nnamdi/transaction/data/transaction.sql
+++ src/main/java/com/nnamdi/transaction/data/transaction.sql
@@ -11,20 +11,28 @@ CREATE TABLE IF NOT EXISTS transaction  (
 
     );
 
-DROP TABLE IF EXISTS Roles;
-CREATE TABLE IF NOT EXISTS Roles (
-                                     role_id INT  PRIMARY KEY AUTO_INCREMENT,
-                                     name VARCHAR(500) NOT NULL
+DROP TABLE IF EXISTS finacialAccount;
+CREATE TABLE IF NOT EXISTS financialAccount (
+                                                financial_account_id INT PRIMARY KEY AUTO_INCREMENT,
+                                                total_balance decimal(10,2) NOT NULL ,
+                                                account_id INT NOT NULL
 
     );
 
-DROP TABLE IF EXISTS user_role;
-CREATE TABLE IF NOT EXISTS UserRole (
-                                        user_role_id INT PRIMARY KEY,
-                                        account_id INT NOT NULL ,
-                                        role_id INT(6) NOT NULL,
-    FOREIGN KEY (account_id) REFERENCES transaction(account_id),
-    FOREIGN KEY (role_id) REFERENCES Roles(role_id)
+ALTER TABLE transaction
+ADD COLUMN date DATETIME;
 
+ALTER TABLE transaction modify transaction_type INT;
 
-    );
+INSERT INTO financial_account( total_balance, account_id) VALUES (1000.00,12345);
+
+# DROP TABLE IF EXISTS user_role;
+# CREATE TABLE IF NOT EXISTS UserRole (
+#                                         user_role_id INT PRIMARY KEY,
+#                                         account_id INT NOT NULL ,
+#                                         role_id INT(6) NOT NULL,
+#     FOREIGN KEY (account_id) REFERENCES transaction(account_id),
+#     FOREIGN KEY (role_id) REFERENCES Roles(role_id)
+#
+#
+#     );
