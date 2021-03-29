e71112fdb6226fec19b438dc537f4957a3e563ec
Nnamdi <nnwabuokei@paga.com>
2021-03-29 15:19:43 +0100

    Moved the file to the right folder

--- src/main/java/com/nnamdi/transaction/service/TransactionRepository.java
+++ src/main/java/com/nnamdi/transaction/service/TransactionRepository.java
@@ -1,7 +1,12 @@
-package com.nnamdi.transaction.controller;
+package com.nnamdi.transaction.service;
 
 import com.nnamdi.transaction.model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+
+
+@Repository
 public interface TransactionRepository extends JpaRepository<Transaction,Long> {
+
 }
