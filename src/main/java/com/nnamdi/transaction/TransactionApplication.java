57aa73f98ca1cc58ddd4cdec0d394136afd5ccfe
Nnamdi <nnwabuokei@paga.com>
2021-03-31 02:55:18 +0100

    Implemented RabbitMqSender, Swagger and application.properties

--- src/main/java/com/nnamdi/transaction/TransactionApplication.java
+++ src/main/java/com/nnamdi/transaction/TransactionApplication.java
@@ -1,13 +1,58 @@
 package com.nnamdi.transaction;
 
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.amqp.support.converter.MessageConverter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TransactionApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TransactionApplication.class, args);
+	}
+	@Value("${spring.rabbitmq.host}")
+	private String host;
+
+	@Value("${spring.rabbitmq.username}")
+	private String username;
+
+	@Value("${spring.rabbitmq.password}")
+	private String password;
+
+	@Value("${rabbitmq.url}")
+	private String rabbitMQUri;
+
+
+
+	@Bean
+	CachingConnectionFactory connectionFactory() {
+		CachingConnectionFactory cachingConnectionFactory = new CachingConnectionFactory(host);
+		cachingConnectionFactory.setUri(rabbitMQUri);
+		cachingConnectionFactory.setRequestedHeartBeat(30);
+		cachingConnectionFactory.setConnectionTimeout(30);
+//		cachingConnectionFactory.setUsername(username);
+//		cachingConnectionFactory.setPassword(password);
+
+		return cachingConnectionFactory;
+	}
+
+
+	@Bean
+	public MessageConverter jsonMessageConverter() {
+		return  new Jackson2JsonMessageConverter();
+	}
+
+	@Bean
+	public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory) {
+		RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+		rabbitTemplate.setMessageConverter(jsonMessageConverter());
+		return rabbitTemplate;
 	}
 
 }
