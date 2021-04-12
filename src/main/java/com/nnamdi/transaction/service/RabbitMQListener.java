55efee77cec638418ca60d1d9347ed67c3ee7c39
Nonso Nwabuokei <nnwabuokei@gmail.com>
2021-04-12 08:40:53 +0100

    Commented RabbitMQListener.java and RabbitMQListener.java which are no longer in use

---
+++ src/main/java/com/nnamdi/transaction/service/RabbitMQListener.java
@@ -0,0 +1,14 @@
+//package com.nnamdi.transaction.service;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.cloud.stream.annotation.EnableBinding;
+//
+//@EnableBinding(MessageService.class)
+//public class RabbitMQListener {
+//    public  static final Logger logger = LoggerFactory.getLogger(RabbitMQListener.class);
+//
+//    public void createFinancialAccount(String msg){
+//
+//    }
+//}
